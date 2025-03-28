687bb2e9bd93aa8549eba11199f3503c2af75260
Zachare Lofton <zachare18@gmail.com>
2025-03-28 10:34:09 -0400

    Feature/thantos finishing up first component (#3)
    * Finsihed my first component
    
    * last commit before merging into master

--- udemy-app/App.js
+++ udemy-app/App.js
@@ -1,13 +1,21 @@
-import { Text, View } from "react-native";
+import { Text, View, Platform } from "react-native";
 import { ProfileCard } from './components/ProfileCard/profile-card';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-import { style } from './app.styles'
+import { style } from './app.styles';
+import { useState } from 'react';
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1, justifyContent: 'center', padding: 20 }}>
-        <ProfileCard firstName={'Codiku'} lastName={'TheDev'} age={30} isOpenToWork={ true }>
+        {Platform.OS === 'android' && <Text>On Android Device</Text>}
+        {Platform.OS === 'ios' && <Text>On Iphone or other IOS device</Text>}
+        <ProfileCard 
+          firstName={'Zach'}
+          lastName={'Lofton'}
+          age={30}
+          isOpenToWork
+        >
          <View style={style.workAvailability} />
         </ProfileCard>
       </SafeAreaView>
--- udemy-app/components/ProfileCard/profile-card.jsx
+++ udemy-app/components/ProfileCard/profile-card.jsx
@@ -1,30 +1,51 @@
-import { TouchableOpacity, Image, View, Text } from 'react-native';
+import { TouchableOpacity, Image, View, Text, Linking } from 'react-native';
 import { profileStyles } from './profile-card.style';
 import { profileCardImageUrl } from './constants/constants';
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome, FontAwesome6 } from '@expo/vector-icons';
 
 export function ProfileCard({ firstName, lastName, age, children }) {
+
+  function goToSocialMedia(socialMedia) {
+    let url;
+
+    switch (socialMedia) {
+      case 'twitter':
+        url = 'http://example.com';
+        break;
+      case 'github':
+        url = 'http://github.com/macka221';
+        break;
+      case 'linkedin':
+        url = 'http://linkedin.com/in/zachare-lofton1997';
+        break;
+      default:
+        break;
+    }
+
+    Linking.openURL(url)
+  }
+
   return (
   <View style={ profileStyles.container }>
       <View style={ profileStyles.header }>
         <View>
           <Image style={ profileStyles.avatar } source={{ uri:  'https://i.pravatar.cc/300' }} />
         </View>
-        <View style={ profileStyles.textContent }> 
+        <View style={ profileStyles.textContent }>
           <Text style={ profileStyles.name }>
             { children } { firstName } { lastName }
           </Text>
-          <Text>Hi I am a { age } React Native Developer, let's get in touch</Text>
+        <Text>Hi I am a { age } React Native Developer, let's get in touch</Text>
         </View>
       </View>
       <View style={ profileStyles.social }>
-        <TouchableOpacity style={ profileStyles.socialButton }>
-          <FontAwesome name='twitter' size={24} color='#1DA1F2' />
+        <TouchableOpacity onPress={ () => goToSocialMedia('twitter') } style={ profileStyles.socialButton }>
+          <FontAwesome6 name='x-twitter' size={24} color='black' />
         </TouchableOpacity>
-        <TouchableOpacity style={ profileStyles.socialButton }>
+        <TouchableOpacity onPress={ () => goToSocialMedia('linkedin') } style={ profileStyles.socialButton }>
           <FontAwesome name='linkedin-square' size={24} color='#0A66C2' />
         </TouchableOpacity>
-        <TouchableOpacity style={ profileStyles.socialButton }>
+        <TouchableOpacity onPress={ () => goToSocialMedia('github') } style={ profileStyles.socialButton }>
           <FontAwesome name='github' size={24} color='#333' />
         </TouchableOpacity>
       </View>
